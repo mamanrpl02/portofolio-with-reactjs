77dee8207b459ca5967a47fd49314e456fb275c5
mamanrpl02 <abdurrahmanhidayatrpl@gmail.com>
2025-08-23 15:40:32 +0700

    all page translate done

--- src/components/About/AboutIntro.jsx
+++ src/components/About/AboutIntro.jsx
@@ -1,23 +1,42 @@
+import { useLang } from "../../context/LangContext";
+
 export default function AboutIntro() {
+  const { lang } = useLang();
+
+  const textEn = {
+    callMe: "Call me Ma",
+    highlighted: "man",
+    paragraph: `I am a passionate 2025 vocational school graduate ready to make a
+      positive impact in the digital world. I am eager to learn, collaborate, and apply my skills to innovative projects.`,
+    linkedin: "Connect on Linkedin",
+  };
+
+  const textId = {
+    callMe: "Panggil saya Ma",
+    highlighted: "man",
+    paragraph: `Saya lulusan SMK 2025 yang penuh semangat, siap memberikan
+      dampak positif di dunia digital. Saya ingin belajar, berkolaborasi, dan mengaplikasikan kemampuan saya pada proyek-proyek inovatif.`,
+    linkedin: "Terhubung di Linkedin",
+  };
+
+  const text = lang === "en" ? textEn : textId;
+
   return (
     <section id="about">
       <div className="contain-about">
         <h1>
-          Call me Ma<span>man</span>
+          {text.callMe}
+          <span>{text.highlighted}</span>
         </h1>
         <div className="paragraft">
-        <p>
-          I am a passionate 2025 vocational school graduate ready to make a
-          positive impact in the digital world. <span>I am eager to learn</span>,
-          collaborate, and apply my skills to innovative projects.
-          </p>
-          </div>
+          <p>{text.paragraph}</p>
+        </div>
         <a
           target="_blank"
           rel="noopener noreferrer"
           href="https://www.linkedin.com/in/abdurrahman-hidayat-581265293/"
         >
-          Connect on Linkedin
+          {text.linkedin}
         </a>
       </div>
     </section>
--- src/components/Sosmed/SocialMedia.jsx
+++ src/components/Sosmed/SocialMedia.jsx
@@ -1,38 +1,51 @@
 /* src/components/Sosmed/SocialMedia.jsx */
 import SosmedItem from "./SosmedItem";
+import { useLang } from "../../context/LangContext";
 
 export default function SocialMedia() {
-  return ( 
-      <section id="manzweb-sosmed" className="sosmed-section">
-        <div className="sosmed-content">
-          <div className="sosmed-title">
-            <h2 className="c-white-1">Find me on social media</h2>
-            <p>
-              Learn more about my activities, journey, and background in the
-            </p>
-          </div>
+  const { lang } = useLang();
 
-          <SosmedItem
-            name="Linkedin"
-            imgSrc="/logo/manzweb-linkedin.png"
-            link="https://www.linkedin.com/in/abdurrahman-hidayat-581265293/"
-          />
-          <SosmedItem
-            name="Instagram"
-            imgSrc="/logo/manzweb-instagram.png"
-            link="https://www.instagram.com/abdurrahman_730/"
-          />
-          <SosmedItem
-            name="Tiktok"
-            imgSrc="/logo/manzweb-tiktok.png"
-            link="https://www.tiktok.com/@abdurrahman_730"
-          />
-          <SosmedItem
-            name="Youtube"
-            imgSrc="/logo/manzweb-youtube.png"
-            link="https://www.youtube.com/@manzweb"
-          />
+  const textEn = {
+    title: "Find me on social media",
+    infor: "Learn more about my activities, journey, and background in the :",
+  };
+
+  const textId = {
+    title: "Temukan saya di media sosial  ",
+    infor: "Pelajari lebih lanjut tentang aktivitas, perjalanan, dan latar belakang saya di:",
+  };
+
+  const text = lang === "en" ? textEn : textId;
+
+  return (
+    <section id="manzweb-sosmed" className="sosmed-section">
+      <div className="sosmed-content">
+        <div className="sosmed-title">
+          <h2 className="c-white-1">{ text.title}</h2>
+          <p>{ text.infor}</p>
         </div>
-      </section> 
+
+        <SosmedItem
+          name="Linkedin"
+          imgSrc="/logo/manzweb-linkedin.png"
+          link="https://www.linkedin.com/in/abdurrahman-hidayat-581265293/"
+        />
+        <SosmedItem
+          name="Instagram"
+          imgSrc="/logo/manzweb-instagram.png"
+          link="https://www.instagram.com/abdurrahman_730/"
+        />
+        <SosmedItem
+          name="Tiktok"
+          imgSrc="/logo/manzweb-tiktok.png"
+          link="https://www.tiktok.com/@abdurrahman_730"
+        />
+        <SosmedItem
+          name="Youtube"
+          imgSrc="/logo/manzweb-youtube.png"
+          link="https://www.youtube.com/@manzweb"
+        />
+      </div>
+    </section>
   );
 }
