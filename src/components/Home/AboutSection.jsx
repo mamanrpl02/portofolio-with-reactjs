992d1aa7bd90eda3ed0ca6204ad9212ad3fb8658
mamanrpl02 <abdurrahmanhidayatrpl@gmail.com>
2025-08-29 12:57:39 +0700

    route component about

--- src/components/Home/AboutSection.jsx
+++ src/components/Home/AboutSection.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function AboutSection({ text, link, linkLabel }) {
   return (
     <section className="section-about">
       <div className="about-container">
         <p>{text}</p>
-        {link && <a href={link}>{linkLabel}</a>}
+        {link && <Link to={link}>{linkLabel}</Link>}
       </div>
     </section>
   );
